75c6ab169218b18a9318a4ff748e82f41a9d4249
cedricgriffiths <cedricgriffiths@10.10.0.26>
2017-08-14 16:03:47 +0100

    1st

---
+++ Dummy/Client.js
@@ -0,0 +1,68 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       14 Aug 2017     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType 
+ * 
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function clientPageInit(type){
+   
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function clientSaveRecord(){
+
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @param {Number} linenum Optional line item number, starts from 1
+ * @returns {Boolean} True to continue changing field value, false to abort value change
+ */
+function clientValidateField(type, name, linenum){
+   
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ * 
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @param {Number} linenum Optional line item number, starts from 1
+ * @returns {Void}
+ */
+function clientFieldChanged(type, name, linenum){
+ 
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to continue line item insert, false to abort insert
+ */
+function clientValidateInsert(type){
+  
+    return true;
+}
