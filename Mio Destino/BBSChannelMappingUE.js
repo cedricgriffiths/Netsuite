0e1c09d8415697341801f15e49fd8835ee7d7679
cedricgriffiths <cedricgriffiths@10.10.0.26>
2017-11-17 15:09:59 +0000

    added code to get the country specific size & description onto the packing slip

--- Mio Destino/BBSChannelMappingUE.js
+++ Mio Destino/BBSChannelMappingUE.js
@@ -89,4 +89,133 @@ function channelMappingUE(type)
 					nlapiSetLineItemValue('item', 'custcol_cseg_bbs_channel', int, noChannel);
 				}
 		}
+	
+	//Look at the channel advisor account id to determine what country specific description & size to use
+	//on forms such as the packing slip
+	//
+	var channelAccountId = nlapiGetFieldValue('custbody_ca_account_id');
+	
+	if(channelAccountId != null && channelAccountId != '')
+		{
+			//Loop through all of the lines on the sales order
+			//
+			var lines = nlapiGetLineItemCount('item');
+			
+			for (var int2 = 1; int2 <= lines; int2++) 
+				{
+					var countryDescription = '';
+					var countrySize = '';
+					
+					//Get the item type & id
+					//
+					var itemType = nlapiGetLineItemValue('item', 'itemtype', int2);
+					var itemId = nlapiGetLineItemValue('item', 'item', int2);
+				
+					//We only want to work with inventory items
+					//
+					if(itemType == 'InvtPart')
+						{
+							//Load up the inventory record
+							//
+							var itemRecord = nlapiLoadRecord('inventoryitem', itemId);
+							var brandSizeId = itemRecord.getFieldValue('custitem_bbs_brand_size_chart');
+							
+							//Load up the brand size record
+							//
+							var brandsizeRecord = null;
+							
+							if(brandSizeId != null && brandSizeId != '')
+								{
+									brandsizeRecord = nlapiLoadRecord('customrecordbbs_gisela_size_chart', brandSizeId);
+								}
+							
+							switch(channelAccountId)
+								{
+									case '72001294':	//United States	
+										countryDescription = itemRecord.getFieldValue('custitem_mio_ca_namerica');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_us_size'));
+										
+										break;
+										
+									case '72001295':	//Germany		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_de');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_de_size'));
+										
+										break;
+												
+									case '12026110':	//Australia		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_title_aa');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_au_size'));
+										
+										break;
+											
+									case '12027738':	//Canada		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_ca_size'));
+										
+										break;
+												
+									case '12033030':	//China		
+										countryDescription = itemRecord.getFieldValue('custitem_mio_ca_chinese');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_size_cn'));
+										
+										break;
+												
+									case '72001762':	//France		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title_fr');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_fr_size'));
+										
+										break;
+												
+									case '12014748':	//Hong Kong		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_hk_size'));
+										
+										break;
+											
+									case '12014747':	//Italy		
+										countryDescription = itemRecord.getFieldValue('custitem_mio_ca_italian');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_it_size'));
+										
+										break;
+												
+									case '12012739':	//Japan		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_jp_size'));
+										
+										break;
+												
+									case '72001223':	//United Kingdom		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_uk_size'));
+										
+										break;
+										
+									case '12032391':	//Malaysia		
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_my_size'));
+										
+										break;
+											
+									case '12015950':	//New Zealand	
+										countryDescription = itemRecord.getFieldValue('custitem_bbs_channel_advisor_title');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_nz_size'));
+										
+										break;
+												
+									case '12014746':	//Spain		
+										countryDescription = itemRecord.getFieldValue('custitem_mio_ca_spanish');
+										countrySize = (brandsizeRecord == null ? '' : brandsizeRecord.getFieldValue('custrecord_bbs_es_size'));
+										
+										break;
+								}
+							
+							//Update the fields on the sales order
+							//
+							nlapiSetLineItemValue('item', 'custcol_bbs_country_specific_desc', int2, countryDescription);
+							nlapiSetLineItemValue('item', 'custcol_bbs_country_specific_size', int2, countrySize);
+
+						}
+				}
+		}
 }
