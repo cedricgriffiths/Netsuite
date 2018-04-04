9f56d97507d29347e2446d11818934998abe30d2
cedricgriffiths <cedricgriffiths@10.10.0.21>
2018-04-04 16:37:33 +0100

    pdf layout changes

--- Abbey Business Group/BBSPrintProjectSuitelet.js
+++ Abbey Business Group/BBSPrintProjectSuitelet.js
@@ -127,22 +127,38 @@ function buildOutput(salesOrderNumber)
 			var salesCustOrderNo = nlapiEscapeXML(salesOrderRecord.getFieldValue('otherrefnum'));
 			var salesOrderDate = salesOrderRecord.getFieldValue('trandate');
 			var salesShipDate = salesOrderRecord.getFieldValue('shipdate');
-			var salesShipInstr = nlapiEscapeXML(salesOrderRecord.getFieldValue('custbody_bbs_dely_instructions'));
+			var salesShipInstr = salesOrderRecord.getFieldValue('custbody_bbs_dely_instructions');
 			var salesNotes = salesOrderRecord.getFieldValue('memo');
 			var salesPhone = salesOrderRecord.getFieldValue('custbody_bbs_so_order_phone');
 			var salesPaymentStatus = nlapiEscapeXML(salesOrderRecord.getFieldValue('custbody_bbs_proj_pay_status'));
 			var salesPaymentTerms = nlapiEscapeXML(salesOrderRecord.getFieldText('custbody_bbs_proj_pay_terms'));
+			var salesSpecialInstr = salesOrderRecord.getFieldValue('custbody_bbs_access_restriction');
+			
 			
 			salesPaymentStatus = (salesPaymentStatus == null ? '' : salesPaymentStatus);
 			salesPaymentTerms = (salesPaymentTerms == null ? '' : salesPaymentTerms);
+			salesShipDate = (salesShipDate == null ? '' : salesShipDate);
+			salesProjectTitle = (salesProjectTitle == null ? '' : salesProjectTitle);
 			
 			var entityRecord = nlapiLoadRecord('customer', salesEntityId);
+			
+			salesSpecialInstr = (salesSpecialInstr == null ? '' : salesSpecialInstr);
+			salesSpecialInstr = nlapiEscapeXML(salesSpecialInstr);
+			salesSpecialInstr = salesSpecialInstr.replace(/\r\n/g,'<br />').replace(/\n/g,'<br />');
+			
+			var newLineCount = (salesSpecialInstr.match(new RegExp("<br />", "g")) || []).length;
+			var linesToAdd = 4 - newLineCount;
+			
+			for (var int = 0; int < linesToAdd; int++) 
+				{
+					salesSpecialInstr += '<br />';
+				}
+			
 			
 			salesShipInstr = (salesShipInstr == null ? '' : salesShipInstr);
 			salesShipInstr = nlapiEscapeXML(salesShipInstr);
 			salesShipInstr = salesShipInstr.replace(/\r\n/g,'<br />').replace(/\n/g,'<br />');
 			
-		
 			salesNotes = (salesNotes == null ? '' : salesNotes);
 			salesNotes = nlapiEscapeXML(salesNotes);
 			salesNotes = salesNotes.replace(/\r\n/g,'<br />').replace(/\n/g,'<br />');
@@ -250,7 +266,7 @@ function buildOutput(salesOrderNumber)
 			xml += "</tr>";
 			
 			xml += "</table>";
-			
+			/*
 			xml += "<table style=\"width: 100%;\">";
 			xml += "<thead>";
 	        xml += "<tr>";
@@ -272,6 +288,7 @@ function buildOutput(salesOrderNumber)
 			xml += "</tr>";
 			
 			xml += "</table>";
+			*/
 			
 			xml += "<table style=\"width: 100%;\">";
 			xml += "<thead>";
@@ -280,7 +297,7 @@ function buildOutput(salesOrderNumber)
 	        xml += "</tr>";
 			xml += "</thead>";
             xml += "<tr>";
-            xml += "<td align=\"left\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;</td>";
+            xml += "<td align=\"left\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + salesSpecialInstr + "</td>";
             xml += "</tr>";
             xml += "<tr>";
 			xml += "<td align=\"left\">&nbsp;</td>";
@@ -328,7 +345,7 @@ function buildOutput(salesOrderNumber)
 							xml += "<tr>";
 							xml += "<td align=\"center\" colspan=\"1\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemQuantity + "</td>";
 							xml += "<td align=\"center\" colspan=\"2\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemCode + "</td>";
-							xml += "<td align=\"left\" colspan=\"10\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemDescription + "</td>";
+							xml += "<td align=\"left\" colspan=\"10\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemGenericDescription + "</td>";
 							xml += "<td align=\"left\" colspan=\"4\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemBaseCode + "</td>";
 							xml += "<td align=\"left\" colspan=\"4\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemBasePo + "</td>";
 							xml += "<td align=\"left\" colspan=\"4\" style=\"font-size:10px; border: 1px solid black; padding: 2px;\">" + itemTopPo + "</td>";
