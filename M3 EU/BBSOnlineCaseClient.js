edf1791580cc12770520fc98a1adbdd3e16338e9
cedricgriffiths <cedricgriffiths@10.10.0.21>
2018-05-31 16:56:19 +0100

    example online case script

---
+++ M3 EU/BBSOnlineCaseClient.js
@@ -0,0 +1,14 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       14 Mar 2018     cedricgriffiths
+ *
+ */
+
+function clientPageInit(type)
+{
+	nlapiSetFieldValue('firstname', 'HardCodedFirstName', false, true);
+	nlapiSetFieldValue('lastname', 'HardCodedLastName', false, true);
+	
+}
