348365569ce7e8e96b2cc8be95c4ffb802c95c67
cedricgriffiths <cedricgriffiths@10.10.0.69>
2018-07-18 16:47:25 +0100

    prevent serial numbered items having a qty > 1 on a sales order line

---
+++ Accora/BBSSOQtyCheckClient.js
@@ -0,0 +1,114 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       18 Jul 2018     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function qtyCheckSaveRecord()
+{
+
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ * 
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @param {Number} linenum Optional line item number, starts from 1
+ * @returns {Void}
+ */
+function qtyCheckFieldChanged(type, name, linenum)
+{
+	if(type == 'item' && name == 'quantity')
+		{
+			var itemId = nlapiGetCurrentLineItemValue('item', 'item');
+			var itemType = nlapiGetCurrentLineItemValue('item', 'itemtype');
+			var itemQty = Number(nlapiGetCurrentLineItemValue('item', 'quantity'));
+			
+			var itemSerialised = itemIsSerialised(itemId, itemType);
+			
+			if(itemSerialised && itemQty > 1)
+				{
+					alert('Item quantity must be "1" for a serial numbered item');
+				}
+		}
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to save line item, false to abort save
+ */
+function qtyCheckValidateLine(type)
+{
+	if(type == 'item')
+	{
+		var itemId = nlapiGetCurrentLineItemValue('item', 'item');
+		var itemType = nlapiGetCurrentLineItemValue('item', 'itemtype');
+		var itemQty = Number(nlapiGetCurrentLineItemValue('item', 'quantity'));
+		
+		var itemSerialised = itemIsSerialised(itemId, itemType);
+		
+		if(itemSerialised && itemQty > 1)
+			{
+				alert('Item quantity must be "1" for a serial numbered item');
+				
+				return false;
+			}
+	}
+	
+    return true;
+}
+
+function itemIsSerialised(item, itemType)
+{
+	var returnValue = false;
+	
+	var isSerialItem = nlapiLookupField(getItemRecordType(itemType), item, 'custitem_serial_numbered', false);
+	
+	if(isSerialItem == 'T')
+		{
+			returnValue = true;
+		}
+	
+	return returnValue;
+}
+
+function getItemRecordType(girtItemType)
+{
+	var girtItemRecordType = '';
+	
+	switch(girtItemType)
+	{
+		case 'InvtPart':
+			girtItemRecordType = 'inventoryitem';
+			break;
+		
+		case 'NonInvtPart':
+			girtItemRecordType = 'noninventoryitem';
+			break;
+		
+		case 'Assembly':
+			girtItemRecordType = 'assemblyitem';
+			break;
+			
+		case 'NonInvtPart':
+			girtItemRecordType = 'noninventoryitem';
+			break;
+	}
+
+	return girtItemRecordType;
+}
+
