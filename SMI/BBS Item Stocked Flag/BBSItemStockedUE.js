fdb6d19eca111ef05e5d862947929405ceaf67f9
cedricgriffiths <cedricgriffiths@10.10.0.69>
2018-08-20 16:53:37 +0100

    new dev to set item stocked flag

---
+++ SMI/BBS Item Stocked Flag/BBSItemStockedUE.js
@@ -0,0 +1,50 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       20 Aug 2018     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ * 
+ * @param {String} type Operation types: create, edit, delete, xedit,
+ *                      approve, cancel, reject (SO, ER, Time Bill, PO & RMA only)
+ *                      pack, ship (IF only)
+ *                      dropship, specialorder, orderitems (PO only) 
+ *                      paybills (vendor payments)
+ * @returns {Void}
+ */
+function itemStockedAfterSubmit(type)
+{
+	if(type == 'create' || type == 'edit')
+		{
+			var newItemRecord = nlapiGetNewRecord();
+			var itemId = newItemRecord.getId();
+			var itemType = newItemRecord.getRecordType();
+			
+			if(itemType == 'inventoryitem')
+				{
+					var lines = newItemRecord.getLineItemCount('locations');
+					var hasReorderPoint = false;
+					
+					for (var int = 1; int <= lines; int++) 
+						{
+							var reorderPoint = newItemRecord.getLineItemValue('locations', 'reorderpoint', int);
+							
+							if(reorderPoint != null && reorderPoint != '')
+								{
+									hasReorderPoint = true;
+									break;
+								}
+						}
+					
+					if(hasReorderPoint)
+						{
+							nlapiSubmitField(itemType, itemId, 'custitem_bbs_item_stocked', 'T', false);
+						}
+				}
+		}
+}
