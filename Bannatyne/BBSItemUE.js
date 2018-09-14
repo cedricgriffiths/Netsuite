6421b484bed8518164e35ddaf09ff70a6fecfb6f
cedricgriffiths <cedricgriffiths@10.10.0.69>
2018-09-14 10:19:35 +0100

    set external id on items

---
+++ Bannatyne/BBSItemUE.js
@@ -0,0 +1,51 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       14 Sep 2018     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ * 
+ * @param {String} type Operation types: create, edit, delete, xedit
+ *                      approve, reject, cancel (SO, ER, Time Bill, PO & RMA only)
+ *                      pack, ship (IF)
+ *                      markcomplete (Call, Task)
+ *                      reassign (Case)
+ *                      editforecast (Opp, Estimate)
+ * @returns {Void}
+ */
+function itemAfterSubmit(type)
+{
+	if(type == 'create' || type == 'edit')
+		{
+			var newRecord = nlapiGetNewRecord();
+			var currentId = newRecord.getId();
+			var currentType = newRecord.getRecordType();
+			
+			var itemRecord = null;
+			
+			try
+				{
+					itemRecord = nlapiLoadRecord(currentType, currentId);
+				}
+			catch(err)
+				{
+					itemRecord = null;
+				}
+			
+			if(itemRecord != null)
+				{
+					var name = itemRecord.getFieldValue('itemid');
+					
+					itemRecord.setFieldValue('externalid', name);
+					
+					nlapiSubmitRecord(itemRecord, false, true);
+				}
+		}
+}
+
+
