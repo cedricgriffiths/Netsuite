d34822a92595bb7acb13a5da163a70bf3811866c
cedricgriffiths <cedricgriffiths@10.10.0.69>
2018-09-27 15:14:17 +0100

    bug fix in po date change select only prefered bins in prod batches

--- SMI/BBS Production Batches/BBSProductionBatchSuitelet.js
+++ SMI/BBS Production Batches/BBSProductionBatchSuitelet.js
@@ -1246,8 +1246,9 @@ function productionBatchSuitelet(request, response)
 					                   //"AND", 
 					                   ["type","anyof","WorkOrd"], 
 					                   "AND", 
-					                   ["custbody_bbs_wo_batch","anyof",batchId]
-					                   
+					                   ["custbody_bbs_wo_batch","anyof",batchId],
+					                   "AND", 
+					                   ["item.preferredbin","is","T"]				                   
 					                ];
 					
 					var transactionSearch = nlapiCreateSearch("transaction", filterArray, 
