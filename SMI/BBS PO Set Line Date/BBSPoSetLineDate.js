ba78f9d431803256ef66cca06600d9b06518d3b4
cedricgriffiths <cedricgriffiths@10.10.0.21>
2018-10-31 16:31:05 +0000

    new code to copy down po date received to the lines

---
+++ SMI/BBS PO Set Line Date/BBSPoSetLineDate.js
@@ -0,0 +1,38 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       31 Oct 2018     cedricgriffiths
+ *
+ */
+
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ * 
+ * @param {String} type Operation types: create, edit, delete, xedit
+ *                      approve, reject, cancel (SO, ER, Time Bill, PO & RMA only)
+ *                      pack, ship (IF)
+ *                      markcomplete (Call, Task)
+ *                      reassign (Case)
+ *                      editforecast (Opp, Estimate)
+ * @returns {Void}
+ */
+function setLineDateBeforeSubmit(type)
+{
+	if(type == 'create')
+		{
+			var receiveByDate = nlapiGetFieldValue('duedate');
+			
+			if(receiveByDate != null && receiveByDate != '')
+				{
+					var lines = nlapiGetLineItemCount('item');
+					
+					for (var int = 1; int <= lines; int++) 
+						{
+							nlapiSetLineItemValue('item', 'expectedreceiptdate', int, receiveByDate);
+						}
+				}
+		}
+}
