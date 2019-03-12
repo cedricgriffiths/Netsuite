25dd46663cafa2092d37e6b1ee2a953428494834
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-03-12 17:04:54 +0000

    script to unset the journal posted flag on copied records

---
+++ TPG/BBS Scheduled Journals/BBSResetJournalPostedUE.js
@@ -0,0 +1,36 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       12 Mar 2019     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Operation types: create, edit, view, copy, print, email
+ * @param {nlobjForm} form Current form
+ * @param {nlobjRequest} request Request object
+ * @returns {Void}
+ */
+function resetJournalPostedBL(type, form, request)
+{
+	if(type == 'copy')
+		{
+			var lines = nlapiGetLineItemCount('item');
+			
+			for (var int = 1; int <= lines; int++) 
+				{
+					nlapiSetLineItemValue('item', 'custcol_bbs_journal_posted', int, 'F');
+				}
+			
+			lines = nlapiGetLineItemCount('expense');
+			
+			for (var int = 1; int <= lines; int++) 
+				{
+					nlapiSetLineItemValue('expense', 'custcol_bbs_journal_posted', int, 'F');
+				}
+		}
+}
