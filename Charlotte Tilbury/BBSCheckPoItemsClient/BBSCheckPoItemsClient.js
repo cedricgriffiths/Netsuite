2506407ce77e9589b6b4e8abeb65ac99e10ca90e
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-03-15 14:55:36 +0000

    new dev for duplicate item detection on po's

---
+++ Charlotte Tilbury/BBSCheckPoItemsClient/BBSCheckPoItemsClient.js
@@ -0,0 +1,45 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       15 Mar 2019     cedricgriffiths
+ *
+ */
+
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to save line item, false to abort save
+ */
+function poItemValidateLine(type)
+{
+	if(type == 'item')
+		{
+			var currentLineItem = nlapiGetCurrentLineItemValue('item', 'item');
+			var currentLineItemText = nlapiGetCurrentLineItemText('item', 'item');
+			var currentLineNo = nlapiGetCurrentLineItemValue('item', 'line');
+			var locationId = nlapiGetFieldValue('location');
+			
+			if(locationId == '11' || locationId == '16')
+				{
+					var lines = nlapiGetLineItemCount('item');
+					
+					for (var int = 1; int <= lines; int++) 
+						{
+							var thisLineItem = nlapiGetLineItemValue('item', 'item', int);
+							var thisLineNo = nlapiGetLineItemValue('item', 'line', int);
+						
+							if(currentLineItem == thisLineItem && currentLineNo != thisLineNo)
+								{
+									alert('WARNING - Product "' + currentLineItemText + '" already exists on this purchase order!')
+									break;
+								}
+						}
+				}
+		}
+
+	return true;
+}
