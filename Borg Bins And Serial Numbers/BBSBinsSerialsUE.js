6d76c73b7c1a876daf29f0f0cdcbf5739242fbb4
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-03-21 17:13:57 +0000

    update so with serial numbers

--- Borg Bins And Serial Numbers/BBSBinsSerialsUE.js
+++ Borg Bins And Serial Numbers/BBSBinsSerialsUE.js
@@ -64,55 +64,104 @@ function binsSerialNumbersBS(type)
 function serialNumbersAS(type)
 {
 	if(type == 'create' || type == 'edit')
-	{
-		var newRecord = nlapiGetNewRecord();
-		var newId = newRecord.getId();
-		var newType = newRecord.getRecordType();
-		var thisRecord = null;
-		
-		try
 		{
-			thisRecord = nlapiLoadRecord(newType, newId);
-		}
-		catch(err)
-		{
-			thisRecord = null;
-		}
-		
-		if(thisRecord)
-			{
-				var count = thisRecord.getLineItemCount('item');
-				
-				for (var int = 1; int <= count; int++) 
+			var newRecord = nlapiGetNewRecord();
+			var newId = newRecord.getId();
+			var newType = newRecord.getRecordType();
+			var thisRecord = null;
+			
+			try
 				{
-					var serials = "";
-					
-					var rec = thisRecord.viewLineItemSubrecord('item', 'inventorydetail', int);
-					
-					if (rec)
-					{
-						var invcount = rec.getLineItemCount('inventoryassignment');  
-						 
-						  for(var x = 1; x <=invcount ; x++) 
-						  {
-							  //rec.selectLineItem('inventoryassignment', x);
-							  //var serialNumber = rec.getCurrentLineItemText('inventoryassignment', 'issueinventorynumber');
-							  var serialNumber = rec.getLineItemText('inventoryassignment', 'issueinventorynumber', x);
-							  
-							  if(x>1)
-							  {
-								  serials += '\n';
-							  }
-							  
-							  serials += serialNumber;
-						  }
-					}
-					
-					thisRecord.setLineItemValue('item', 'custcol_bbs_if_serial_no', int, serials);
+					thisRecord = nlapiLoadRecord(newType, newId);
 				}
-				
-				nlapiSubmitRecord(thisRecord, false, true);
-			}
-	}
+			catch(err)
+				{
+					thisRecord = null;
+				}
+			
+			if(thisRecord)
+				{
+					var count = thisRecord.getLineItemCount('item');
+					var salesOrderId = thisRecord.getFieldValue('createdfrom');
+					var thisRecordUpdated = false;
+					
+					for (var int = 1; int <= count; int++) 
+						{
+							var serials = "";
+							var itemOrderLine = thisRecord.getLineItemValue('item', 'orderline', int);
+							var rec = thisRecord.viewLineItemSubrecord('item', 'inventorydetail', int);
+							var serialCount = Number(0);   
+							
+							if (rec)
+								{
+									var invcount = rec.getLineItemCount('inventoryassignment');  
+									 
+									  for(var x = 1; x <=invcount ; x++) 
+										  {
+											  var serialNumber = rec.getLineItemText('inventoryassignment', 'issueinventorynumber', x);
+											  
+											  if(x>1)
+												  {
+													  serials += '\n';
+												  }
+											  
+											  serials += serialNumber;
+											  serialCount++;
+										  }
+								}
+							
+							
+							if(serialCount > 0)
+								{
+									thisRecord.setLineItemValue('item', 'custcol_bbs_if_serial_no', int, serials);
+									thisRecordUpdated = true;
+									
+									//Update the sales order with the serial numbers
+									//
+									var salesOrderRecord = null;
+									
+									try
+										{
+											salesOrderRecord = nlapiLoadRecord('salesorder', salesOrderId);
+										}
+									catch(err)
+										{
+											salesOrderRecord = null;
+										}
+									
+									if(salesOrderRecord != null)
+										{
+											var soLines = salesOrderRecord.getLineItemCount('item');
+											
+											for (var int2 = 1; int2 <= soLines; int2++) 
+												{
+													var soLineNo = salesOrderRecord.getLineItemValue('item', 'line', int2);
+													
+													if(soLineNo == itemOrderLine)
+														{
+															salesOrderRecord.setLineItemValue('item', 'custcol_bbs_if_serial_no', int2, serials);
+															
+															try
+																{
+																	nlapiSubmitRecord(salesOrderRecord, false, true);
+																}
+															catch(err)
+																{
+																	nlapiLogExecution('ERROR', 'Error saving sales order', err.message);
+																}
+															
+															break;
+														}
+												}
+										}
+								}
+						}
+					
+					if(thisRecordUpdated)
+						{
+							nlapiSubmitRecord(thisRecord, false, true);
+						}
+				}
+		}
 }	
 
